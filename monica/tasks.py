3c985f472502964f7c025961c3e7b7e9303dc969
Sam Hill <samhilll@Sams-MacBook-Pro.local>
2023-01-10 23:06:17 -0500

    add task endpoint

---
+++ monica/tasks.py
@@ -0,0 +1,103 @@
+"""
+@author: Sam Hill
+
+Offical Documentation: https://www.monicahq.com/api/activities
+
+Description: We will put all task related requests here
+
+
+"""
+
+import time
+import requests
+import json
+from .utils import Utils
+from pprint import pprint
+import pandas as pd
+
+basic_api = 'https://app.monicahq.com/api'
+
+
+class Tasks:
+	def __init__(self, access_token, wait_time=1):
+		"""
+		Connect with monica tasks API found at https://www.monicahq.com/api/tasks
+
+		Parameters: 
+		-----------
+			access_token: str
+				token retreived from monica platform
+
+			wait_time: int
+				seconds to wait after every request sent
+
+		"""
+		headers = {'Authorization': f'Bearer {access_token}', 
+					'Content-type': 'application/json', 
+					'Accept': 'text/plain'}
+		
+		self.headers = headers	
+		self.basic_api = basic_api
+		self.wait_time = wait_time
+		self.utils = Utils()
+
+	def list_tasks(self):
+		"""
+		Gets the tasks from monica. Checkout monica API documentation for detailed description.
+
+		Parameters: None
+		-----------
+
+		
+		Returns: 
+		-------
+		json_data: dict/json
+			can be easily converted to pandas dataframe	
+
+		"""
+		headers = self.headers
+		wait_time = self.wait_time
+		basic_api = self.basic_api		
+
+		api = f"{basic_api}/tasks"
+
+		response = requests.get(api, headers=headers)    
+		print(response)
+
+		json_data = response.json()
+
+		return json_data
+
+
+    def add_task(self, title, description, contact_id):
+		headers = self.headers
+		wait_time = self.wait_time
+		basic_api = self.basic_api		
+
+		api = f"{basic_api}/task/"
+		payload = {
+			"title": title,
+			"description": description,
+			"contact_id": contact_id
+		}
+		response = requests.put(api, params=payload, headers=headers)    
+		print(response)
+
+		json_data = response.json()
+
+		return json_data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
